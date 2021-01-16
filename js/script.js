9c0408c488a813e013910524121b983274850cc6
John HC <69842878+Purgatos81@users.noreply.github.com>
2021-01-16 10:03:05 +0100

    Started work

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//# Programming Foundations Module Assignment 2
+//Question 1
+
+var pets = [
+	{
+		type: "cat",
+		age: 5.5,
+	},
+	{
+		type: "dog",
+		age: 3.8,
+	},
+	{
+		type: "parrot",
+		age: 4.0,
+	},
+];
+
+for (var i = 0; i < pets.length; i++) {
+    var petAge = pets[i];
+    console.log(petAge);
+}
+
